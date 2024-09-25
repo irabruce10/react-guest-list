bc6daf6b3d8491a656714888c7281e5b8fdcc47e
Bruce <irabruce10@gmail.com>
2024-09-25 17:08:31 +0200

    Creating some file

--- src/App.js
+++ src/App.js
@@ -5,19 +5,49 @@ export default function App() {
 
   const [firstName, setFirstName] = useState('');
 
+  const [users, setUsers] = useState([]);
+
+  const userHandleSubmit = (event) => {
+    event.preventDefault();
+    setUsers([...users, { firstName, lastName }]);
+    setFirstName('');
+    setLastName('');
+    console.log('User');
+  };
+
   return (
     <div>
-      <h1></h1>
-      <form>
+      {users.map((user) => {
+        return (
+          <h1 key={user.firstName}>
+            {user.firstName} {user.lastName}
+          </h1>
+        );
+      })}
+      <h1>Guest List</h1>
+      <form onSubmit={userHandleSubmit}>
         <label>
           First name
-          <input type="text" name="name" placeholder="First Name" />
+          <input
+            type="text"
+            name="name"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
         </label>
         <br />
         <label>
           Last name
-          <input type="text" name="name" placeholder="Last Name" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
         </label>
+        <button type="submit">Create user</button>
       </form>
     </div>
   );
